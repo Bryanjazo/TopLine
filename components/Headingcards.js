e7be5809342d84768f0d5b7695c4db52fb2cc840
Bryan Jazo <Bryanjazo88@gmail.com>
2022-03-03 08:48:53 -0800

    uodating records

--- components/Headingcards.js
+++ components/Headingcards.js
@@ -11,50 +11,142 @@ import {
 import pressureWasher from "../public/Images/pressure.png";
 export default function Headingcards() {
   return (
-    <div className={styles.row}>
-      <div className={styles.card_box_1}>
-        <div className={styles.block}>
-          <FontAwesomeIcon icon={faUsers} size="5x" color="white" />
-        </div>
-        <div className={styles.textblock}>
-          <h3 className={styles.headingTertiary}>Professional Team</h3>
-          <p className={styles.box__text}>
-            TopLine provides professional service. Our costumers come first.
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.card_box_2}>
-        <div className={styles.block}>
-          <div className={styles.image}>
-            <FontAwesomeIcon
-              icon={faAirFreshener}
-              className={styles.icon}
-              size="5x"
-            />
+    <section className={styles.sectionCards}>
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <div className={styles.wrap}>
+              <div className={styles.widget_wrap}>
+                <div className={styles.widget}>
+                  <div className={styles.widget_container}>
+                    <section className={styles.about_area}>
+                      <div className={styles.outer_container}>
+                        {/* no scss */}
+                        <div className={styles.feature_section}>
+                          {/* scss */}
+                          <div className={styles.auto_container}>
+                            <div className={styles.inner__container}>
+                              <div className={styles.clearfix}>
+                                <div className={styles.feature_column_1}>
+                                  <div className={styles.feature_block}>
+                                    <div className={styles.inner_box}>
+                                      <div className={styles.icon_box}>
+                                        <FontAwesomeIcon
+                                          icon={faUsers}
+                                          size="5x"
+                                          color="white"
+                                          className={styles.icon}
+                                        />
+                                        <h4>Professional Team</h4>
+                                        <p>
+                                          TopLine provides professional service.
+                                          Our costumers come first.
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className={styles.feature_column_2}>
+                                  <div className={styles.feature_block}>
+                                    <div className={styles.inner_box}>
+                                      <div className={styles.icon_box}>
+                                        <FontAwesomeIcon
+                                          icon={faUsers}
+                                          size="5x"
+                                          color="red"
+                                          className={styles.icon}
+                                        />
+                                        <h4>Professional Team</h4>
+                                        <p>
+                                          TopLine provides professional service.
+                                          Our costumers come first.
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className={styles.feature_column_3}>
+                                  <div className={styles.feature_block}>
+                                    <div className={styles.inner_box}>
+                                      <div className={styles.icon_box}>
+                                        <FontAwesomeIcon
+                                          icon={faUsers}
+                                          size="5x"
+                                          color="white"
+                                          className={styles.icon}
+                                        />
+                                        <h4>Professional Team</h4>
+                                        <p>
+                                          TopLine provides professional service.
+                                          Our costumers come first.
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </section>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className={styles.textblock}>
-          <h3 className={styles.headingTertiary}>Quality Washing</h3>
-          <p className={styles.box__text}>
-            We make sure that our team gives every costumer a 100% satisfaction
-            guaranteed service.
-          </p>
-        </div>
       </div>
-
-      <div className={styles.card_box_3}>
-        <div className={styles.block}>
-          <FontAwesomeIcon icon={faFileInvoiceDollar} size="5x" color="white" />
-        </div>
-        <div className={styles.textblock}>
-          <h3 className={styles.headingTertiary}>Free Estimate</h3>
-          <p className={styles.box__text}>
-            We provide our service at a much more affordable price with amazing
-            results.
-          </p>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 }
+
+// <div className={styles.row}>
+// <div className={styles.card_box_1}>
+//   <div className={styles.block}>
+//     <FontAwesomeIcon icon={faUsers} size="5x" color="white" />
+//   </div>
+//   <div className={styles.textblock}>
+//     <h3 className={styles.headingTertiary}>Professional Team</h3>
+//     <p className={styles.box__text}>
+//       TopLine provides professional service. Our costumers come first.
+//     </p>
+//   </div>
+// </div>
+
+// <div className={styles.card_box_2}>
+//   <div className={styles.block}>
+//     <div className={styles.image}>
+//       <FontAwesomeIcon
+//         icon={faAirFreshener}
+//         className={styles.icon}
+//         size="5x"
+//       />
+//     </div>
+//   </div>
+//   <div className={styles.textblock}>
+//     <h3 className={styles.headingTertiary}>Quality Washing</h3>
+//     <p className={styles.box__text}>
+//       We make sure that our team gives every costumer a 100%
+//       satisfaction guaranteed service.
+//     </p>
+//   </div>
+// </div>
+
+// <div className={styles.card_box_3}>
+//   <div className={styles.block}>
+//     <FontAwesomeIcon
+//       icon={faFileInvoiceDollar}
+//       size="5x"
+//       color="white"
+//     />
+//   </div>
+//   <div className={styles.textblock}>
+//     <h3 className={styles.headingTertiary}>Free Estimate</h3>
+//     <p className={styles.box__text}>
+//       We provide our service at a much more affordable price with
+//       amazing results.
+//     </p>
+//   </div>
+// </div>
+// </div>
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -20,70 +20,54 @@ import EmailCard from "./EmailCard/EmailCard";
 export default function HomeComponent() {
   return (
     <div>
-      <div className={styles.home_container}>
-        <div className={styles.home}>
-          <div className={styles.home_logo}>
-            <Image
-              src={logo}
-              alt="Picture of the author"
-              width={200}
-              height={100}
-            />
-          </div>
-
-          <div className={styles.socialIcons}>
-            <ul>
-              <li>
-                <span>
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className={styles.icon}
-                    size="3x"
-                  />
-                  <h2 className={styles.topText}>Contact Us Now</h2>
-                </span>
-                <a
-                  className={styles.text}
-                  href="mailto:Toplinepowerwash@gmail.com"
-                >
-                  Toplinepowerwash@gmail.com
-                </a>
-              </li>
-              <li>
-                {" "}
-                <span>
-                  <FontAwesomeIcon
-                    icon={faPhoneAlt}
-                    className={styles.icon}
-                    size="3x"
-                  />
-                  <h2 className={styles.topText}>Call Us Now</h2>
-                </span>
-                <a className={styles.phone} href="tel:9097091148">
-                  (909)-709-1148
-                </a>
-              </li>
-              <li>
-                {" "}
-                <span>
-                  <FontAwesomeIcon
-                    icon={faMapPin}
-                    className={styles.icon}
-                    size="3x"
-                  />
-                  <h2 className={styles.topText}>Where were located</h2>
-                </span>
-                <a
-                  className={(styles.text, styles.location)}
-                  href="https://g.co/kgs/25yBJt"
-                >
-                  Ontario, Ca
-                </a>
-              </li>
-            </ul>
+      <header className={styles.mainHeader}>
+        <div className={styles.headerUpper}>
+          <div className={styles.autoContainer}>
+            <div className={styles.clearfix}>
+              <div className={styles.pull_left}>
+                <div className={styles.logo_box}>
+                  <div className={styles.bgColor}></div>
+                  <figure className={styles.logo}>
+                    <div className={styles.l}>
+                      <Image
+                        src={logo}
+                        alt="Picture of the author"
+                        width={200}
+                        height={100}
+                      />
+                    </div>
+                  </figure>
+                </div>
+              </div>
+              <div className={styles.pull_right}>
+                <ul className={styles.info_list}>
+                  <li>
+                    <FontAwesomeIcon
+                      icon={faEnvelope}
+                      className={styles.icon}
+                    />
+                    <h2>Contact Us</h2>
+                    <p>ToplineWash</p>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faMapPin} className={styles.icon} />
+                    <h2>Location</h2>
+                    <p>Ontario, CA</p>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon
+                      icon={faPhoneAlt}
+                      className={styles.icon}
+                    />
+                    <h2>Call Today</h2>
+                    <p>909-709-1148</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </header>
       <MainHomeHeading />
       <Headingcards />
       <PopupMidsection />
@@ -93,3 +77,68 @@ export default function HomeComponent() {
     </div>
   );
 }
+
+// <div className={styles.home_container}>
+// <div className={styles.home}>
+//   <div className={styles.home_logo}>
+//     <Image
+//       src={logo}
+//       alt="Picture of the author"
+//       width={200}
+//       height={100}
+//     />
+//   </div>
+
+//   <div className={styles.socialIcons}>
+//     <ul>
+//       <li>
+//         <i>
+//           <FontAwesomeIcon
+//             icon={faEnvelope}
+//             className={styles.icon}
+//             size="3x"
+//           />
+//           <h5 className={styles.topText}>Contact Us Now</h5>
+//         </i>
+//         <a
+//           className={styles.text}
+//           href="mailto:Toplinepowerwash@gmail.com"
+//         >
+//           Toplinepowerwash@gmail.com
+//         </a>
+//       </li>
+//       <li>
+//         {" "}
+//         <i>
+//           <FontAwesomeIcon
+//             icon={faPhoneAlt}
+//             className={styles.icon}
+//             size="3x"
+//           />
+//           <h5 className={styles.topText}>Call Us Now</h5>
+//         </i>
+//         <a className={styles.phone} href="tel:9097091148">
+//           (909)-709-1148
+//         </a>
+//       </li>
+//       <li>
+//         {" "}
+//         <i>
+//           <FontAwesomeIcon
+//             icon={faMapPin}
+//             className={styles.icon}
+//             size="3x"
+//           />
+//           <h5 className={styles.topText}>Where were located</h5>
+//         </i>
+//         <a
+//           className={(styles.text, styles.location)}
+//           href="https://g.co/kgs/25yBJt"
+//         >
+//           Ontario, Ca
+//         </a>
+//       </li>
+//     </ul>
+//   </div>
+// </div>
+// </div>
